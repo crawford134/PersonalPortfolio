3f05363466813a94b6f6179a0e12c2c770daaf4f
Kiley Carson <38968995+crawford134@users.noreply.github.com>
2024-03-29 18:16:15 -0600

    Template import (#38)
    * updated images
    
    * template import

---
+++ src/Components/Header.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+import ParticlesBg from "particles-bg";
+import Fade from "react-reveal";
+
+class Header extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const project = this.props.data.project;
+    const github = this.props.data.github;
+    const name = this.props.data.name;
+    const description = this.props.data.description1;
+    const description2 = this.props.data.description2;
+
+    let config = {
+      num: 1,
+      rps: 2,
+      radius: 20,
+      life: [5, 6],
+      v: [2, 3],
+      tha: [-40, 40],
+      alpha: [0.6, 0],
+      scale: 10,
+      position: "all",
+      color: ["#311B92","#1A237E","#880E4F"],
+      // cross: "dead",
+      // emitter: "follow",
+      random: 1
+    };
+
+    return (
+      <header id="home">
+        <ParticlesBg num={200} type="circle" bg={true} />
+
+        <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+            Show navigation
+          </a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">
+            Hide navigation
+          </a>
+
+          <ul id="nav" className="nav">
+            <li className="current">
+              <a className="smoothscroll" href="#home">
+                Home
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#about">
+                About
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#resume">
+                Resume
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="row banner">
+          <div className="banner-text">
+            <Fade bottom>
+              <h1 className="responsive-headline">{name}</h1>
+            </Fade>
+            <Fade bottom duration={1200}>
+              <h3>{description}</h3>
+              <h3>{description2}</h3>
+            </Fade>
+            <hr />
+            <Fade bottom duration={2000}>
+              <ul className="social">
+                <a href={project} className="button btn project-btn">
+                  <i className="fa fa-book"></i>Project
+                </a>
+                <a href={github} className="button btn github-btn">
+                  <i className="fa fa-github"></i>Github
+                </a>
+              </ul>
+            </Fade>
+          </div>
+        </div>
+
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p>
+      </header>
+    );
+  }
+}
+
+export default Header;
