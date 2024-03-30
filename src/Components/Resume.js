f4de5c4033c2381099edbdce27e94314558f53db
Kiley Carson <38968995+crawford134@users.noreply.github.com>
2024-03-29 21:28:44 -0600

    updated website content (#39)

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -24,11 +24,29 @@ class Resume extends Component {
             <em className="date">{education.graduated}</em>
           </p>
           <p>{education.description}</p>
+          {education.link_type ?
+          <a className="button btn resume-btn" href={education.link} >View {education.link_type}</a>
+          :
+          <></>
+          },
         </div>
       );
     });
 
     const work = this.props.data.work.map(function (work) {
+      return (
+        <div key={work.company}>
+          <h3>{work.company}</h3>
+          <p className="info">
+            {work.title}
+            <span>&bull;</span> <em className="date">{work.years}</em>
+          </p>
+          <p>{work.description}</p>
+        </div>
+      );
+    });
+
+    const volunteering = this.props.data.volunteering.map(function (work) {
       return (
         <div key={work.company}>
           <h3>{work.company}</h3>
@@ -81,6 +99,18 @@ class Resume extends Component {
             </div>
 
             <div className="nine columns main-col">{work}</div>
+          </div>
+        </Slide>
+
+        <Slide left duration={1300}>
+          <div className="row work">
+            <div className="three columns header-col">
+              <h1>
+                <span>Volunteering</span>
+              </h1>
+            </div>
+
+            <div className="nine columns main-col">{volunteering}</div>
           </div>
         </Slide>
 
